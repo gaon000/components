96b7f8d63888d54aa537a4e778757e2c63047e6a
gaon000 <gaon000@naver.com>
2021-12-31 20:55:56 +0900

    add transaction

--- back/src/controllers/post.controller.js
+++ back/src/controllers/post.controller.js
@@ -2,45 +2,69 @@ import postTable from "../databases/postTable";
 import models from "../models";
 
 const getPostList = async (req, res, next) => {
+  let transaction = null;
   try {
-    const transaction = models.sequelize.transaction();
+    transaction = models.sequelize.transaction();
     //TODO
     //const { page, limit, category } = req.query
-    const { subject } = req.query
+    const { subject } = req.query;
     const condition = {
       isDeleted: false,
-    }
+    };
     if (subject !== "all") {
-      condition.subject = subject
+      condition.subject = subject;
     }
     const postList = await postTable.getPostList(condition);
-    transaction.commit();
-    return res.status(200);
+    await transaction.commit();
+    return res.status(200).json({ message: "ok", result: postList });
   } catch (err) {
-    transaction.rollback();
+    await transaction.rollback();
+    return res.status(500).json({ message: "bad" });
   }
 };
 
 const createPost = async (req, res, next) => {
+  let transaction = null;
   try {
-    const transaction = models.sequelize.transaction();
-    const { title, contents, writer, subject}
-  } catch (err) {}
+    transaction = models.sequelize.transaction();
+    const { title, contents, writer, subject } = req.body;
+    await postTable.store({ title, contents, writer, subject });
+    await transaction.commit();
+    return res.status(200).json({ message: "ok" });
+  } catch (err) {
+    await transaction.rollback();
+    return res.status(500).json({ message: "bad" });
+  }
 };
 
 const getPost = async (req, res, next) => {
+  let transaction = null;
   try {
-  } catch (err) {}
+    transaction = models.sequelize.transaction();
+    await transaction.commit();
+  } catch (err) {
+    await transaction.rollback();
+  }
 };
 
 const modifyPost = async (req, res, next) => {
+  let transaction = null;
   try {
-  } catch (err) {}
+    transaction = models.sequelize.transaction();
+    await transaction.commit();
+  } catch (err) {
+    await transaction.rollback();
+  }
 };
 
 const deletePost = async (req, res, next) => {
+  let transaction = null;
   try {
-  } catch (err) {}
+    transaction = models.sequelize.transaction();
+    await transaction.commit();
+  } catch (err) {
+    await transaction.rollback();
+  }
 };
 
 export { getPostList, getPost, createPost, modifyPost, deletePost };
