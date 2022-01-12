7e31ceda190bfa271f0f20d3ade3a84d08a0ce27
gaon000 <gaon000@naver.com>
2022-01-12 23:49:56 +0900

    allocate class instance

--- back/src/app.js
+++ back/src/app.js
@@ -29,14 +29,10 @@ app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  console.log(err.message);
 
-  // render the error page
-  res.status(err.status || 500);
+  return res.status(400).json({ message: err.message });
 });
 
 module.exports = app;
--- back/src/controllers/post.controller.js
+++ back/src/controllers/post.controller.js
@@ -7,6 +7,7 @@ const getPostList = async (req, res, next) => {
   let transaction = null;
   try {
     transaction = await models.sequelize.transaction();
+    const Post = new postTable();
     //TODO
     //const { page, limit, category } = req.query
     const { subject } = req.query;
@@ -16,7 +17,7 @@ const getPostList = async (req, res, next) => {
     if (subject !== "all") {
       condition.subject = subject;
     }
-    const postList = await postTable.getPostList(condition);
+    const postList = await Post.getPostList(condition);
     await transaction.commit();
     return res.status(200).json({ message: "ok", result: postList });
   } catch (err) {
@@ -29,11 +30,12 @@ const createPost = async (req, res, next) => {
   let transaction = null;
   try {
     transaction = await models.sequelize.transaction();
+    const Post = new postTable();
     const { title, contents, subject } = req.body;
     if (title === undefined || contents === undefined || subject === undefined)
       throw createError(httpStatus.BAD_REQUEST, "INVALID PARAMETER");
 
-    await postTable.store({ title, contents, subject });
+    await Post.store({ title, contents, subject });
     await transaction.commit();
     return res.status(201).json({ message: "ok" });
   } catch (err) {
@@ -46,9 +48,10 @@ const getPost = async (req, res, next) => {
   let transaction = null;
   try {
     transaction = await models.sequelize.transaction();
+    const Post = new postTable();
     const { postId } = req.params;
     console.log(postId);
-    const postContents = await postTable.getPost(postId);
+    const postContents = await Post.getPost(postId);
     if (postContents === null)
       throw createError(httpStatus.BAD_REQUEST, "INVALID POST ID");
     await transaction.commit();
@@ -63,11 +66,12 @@ const modifyPost = async (req, res, next) => {
   let transaction = null;
   try {
     transaction = await models.sequelize.transaction();
+    const Post = new postTable();
     const { postId } = req.params;
     const { title, contents, subject } = req.body;
     if (title === undefined || contents === undefined || subject === undefined)
       throw createError(httpStatus.BAD_REQUEST, "INVALID PARAMETER");
-    await postTable.modifyPost(postId, { title, contents, subject });
+    await Post.modifyPost(postId, { title, contents, subject });
     await transaction.commit();
     return res.status(200);
   } catch (err) {
@@ -80,8 +84,9 @@ const deletePost = async (req, res, next) => {
   let transaction = null;
   try {
     transaction = await models.sequelize.transaction();
+    const Post = new postTable();
     const { postId } = req.params;
-    await postTable.deletePost(postId);
+    await Post.deletePost(postId);
     await transaction.commit();
     return res.status(200);
   } catch (err) {
